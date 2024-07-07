5efcb76910c2bd74fba674dd4b7fce630b63710b
Junx2001 <ratsirarsonj@gmail.com>
2024-07-07 15:06:27 +0300

    [Product] Model

---
+++ models/product.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define('Product', {
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      product_image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      is_exchangeable: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      first_owner_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+    });
+  
+    return Product;
+  };
