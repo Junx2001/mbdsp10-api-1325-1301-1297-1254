fa259751403258ce8b74bfd1f96faa08fe109db6
Junx2001 <ratsirarsonj@gmail.com>
2024-08-11 21:45:30 +0300

    [Config] Add Port To POSTGRES

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT,
   
   mongo_db_host: process.env.MONGO_DB_HOST,
