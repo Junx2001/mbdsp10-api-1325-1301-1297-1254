c08ef2bf00bd09efba68916740122575afada357
Junx2001 <ratsirarsonj@gmail.com>
2024-08-11 22:17:27 +0300

    [Config] Config Mongo DB Connection

--- models/mongo_models/mongoDb.js
+++ models/mongo_models/mongoDb.js
@@ -2,6 +2,12 @@
 const mongoose = require('mongoose');
 const dbConfig = require('../../config/config');
 
-mongoose.connect('mongodb://'+dbConfig.mongo_db_host+':'+dbConfig.mongo_db_port+'/'+dbConfig.mongo_db_name);
+if(dbConfig.port){
+    mongoose.connect('mongodb://'+dbConfig.mongo_db_host+':'+dbConfig.mongo_db_port+'/'+dbConfig.mongo_db_name);
+}
+else{
+    mongoose.connect('mongodb+srv://'+dbConfig.mongo_db_host+'/'+dbConfig.mongo_db_name);
+}
+
 
 module.exports = mongoose;
