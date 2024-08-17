36cca04186703ded46f16cf0c4eaed7639192357
Junx2001 <ratsirarsonj@gmail.com>
2024-08-17 20:59:59 +0300

    [Exchanges] Some Fixes on Accept and Cancel

---
+++ services/exchange-service.js
@@ -0,0 +1,67 @@
+
+const db = require('../models/pg_models');
+const { Exchange, Proposition, Product } = db;
+const { Op } = require('sequelize');
+
+
+
+const acceptExchangeUpdates = async (exchange) => {
+    exchange.status = 'ACCEPTED';
+    await exchange.save();
+
+    // Reload the exchange to get all the propositions with the products
+    await exchange.reload({
+        include: [
+          { model: Proposition, as: 'owner_proposition', include: [Product] },
+          { model: Proposition, as: 'taker_proposition', include: [Product] }
+        ]
+      }).then(async () => {
+        // Get all the products involved in the exchange;
+        const ownerProducts = exchange.owner_proposition.Products;
+        const takerProducts = exchange.taker_proposition.Products;
+
+        // Update all actual_owner_id of the products involved in the exchange
+        await Product.update({ actual_owner_id: exchange.taker_proposition.user_id }, {
+          where: { id: { [Op.in]: ownerProducts.map(product => product.id) } }
+        });
+        await Product.update({ actual_owner_id: exchange.owner_proposition.user_id }, {
+            where: { id: { [Op.in]: takerProducts.map(product => product.id) } }
+        });
+        
+        // Deactivate all propositions where products involved in the exchange
+        await Proposition.update({ is_active: false }, {
+          where: { id: { [Op.in]: ownerProducts.map(product => product.id) },
+                    id : { [Op.ne]: exchange.owner_proposition.id} }
+        });
+        await Proposition.update({ is_active: false }, {
+          where: { id: { [Op.in]: takerProducts.map(product => product.id) },
+                    id : { [Op.ne]: exchange.taker_proposition.id} }    
+        });
+  
+        const updatedPropositions = await Proposition.findAll({
+          where: { id: { [Op.in]: [...ownerProducts.map(product => product.id), ...takerProducts.map(product => product.id)] } },
+          include: [Product]
+        });
+  
+        // Block all exchanges involving propositions with the products involved in the exchange
+        await Exchange.update({ status: 'BLOCKED' }, {
+          where: {
+            id: { [Op.ne]: exchange.id },
+            status: 'PENDING',
+            [Op.or]: [
+              { owner_proposition_id: { [Op.in]: updatedPropositions.map(proposition => proposition.id) } },
+              { taker_proposition_id: { [Op.in]: updatedPropositions.map(proposition => proposition.id) } }
+            ]
+          }
+        });
+      });
+
+      
+
+}
+
+
+
+module.exports = {
+    acceptExchangeUpdates
+};
